f27c07fbbb81bf2d42bfe1f54c70f7e2ea0e4dd3
Boris Djurdjevic <boris@djurdjevic.ch>
2017-03-24 11:12:26 +0100

    beginning implementing sound tracking

--- README.md
+++ README.md
@@ -3,15 +3,20 @@ Konstanten
     Schallgeschwindigkeit: 343 m/s
     ATmega328p Frequenz: 16 MHz
     ADC Vorteiler: 16
+    ADC Conversion Cycles: 13
 
 ADC Clock: 1 MHz (50-200 KHz = Maximum resolution)
+ADC Conversion Time: 0.013ms 
 
 Schall Weg/Zeit:
 
-    1.0m = 29.1545189ms
-    0.5m = 14.57725945ms
+    1.00m = 29.1545189ms
+    0.50m = 14.57725945ms
+    0.01m = 0.291545189ms
+
 
 # Links
 https://www.arduino.cc/en/Hacking/Atmega168Hardware
 https://de.wikibooks.org/wiki/C-Programmierung_mit_AVR-GCC/_IO-Ports
 http://www.atmel.com/webdoc/avrlibcreferencemanual/group__avr__sfr_1gaaf6857fa882da35f8685e2001e5c3bbe.html
+http://www.atmel.com/images/Atmel-8271-8-bit-AVR-Microcontroller-ATmega48A-48PA-88A-88PA-168A-168PA-328-328P_datasheet_Complete.pdf
--- prog.c
+++ prog.c
@@ -22,6 +22,20 @@ uint16_t adc_read(uint8_t chan) {
 	return ADCW;
 }
 
+// Initialize 16-Bit timer
+void timer1_init(void) {
+	TCCR1B = _BV(CS10); // Prescaler: 16MHz/1 = 16 MHz
+	TCNT1 = 0; // Initialize to zero
+}
+
+inline void timer1_reset(void) {
+	TCNT1 = 0;
+}
+
+inline uint16_t timer1_get(void) {
+	return TCNT1;
+}
+
 uint16_t inpbuf[INPBUF_LEN];
 
 void recordInpbuf(uint8_t chan) {
@@ -37,23 +51,82 @@ void recordInpbuf(uint8_t chan) {
 	puts("\n");
 }
 
+#define BREAKOUT_LEVEL 600
+
+void distMeter() {
+	uint16_t inp_0_ref = 0;
+	uint16_t inp_1_ref = 0;
+	uint16_t inp_2_ref = 0;
+	uint8_t inp_0_set = 0;
+	uint8_t inp_1_set = 0;
+	uint8_t inp_2_set = 0;
+	uint8_t timing = 0;
+
+	for (;;) {
+		uint16_t inp_0 = adc_read(0);
+		uint16_t inp_1 = adc_read(1);
+		uint16_t inp_2 = adc_read(2);
+
+		if (inp_0 >= BREAKOUT_LEVEL && !inp_0_set) {
+			// inp_0 breakout
+			if (!timing) {
+				timer1_reset();
+				timing = 1;
+			}
+
+			inp_0_ref = timer1_get();
+			inp_0_set = 1;
+
+		} else if (inp_1 >= BREAKOUT_LEVEL && !inp_1_set) {
+			// inp_1 breakout
+			if (!timing) {
+				timer1_reset();
+				timing = 1;
+			}
+
+			inp_1_ref = timer1_get();
+			inp_1_set = 1;
+		} else if (inp_2 >= BREAKOUT_LEVEL && !inp_2_set) {
+			// inp_2 breakout
+			if (!timing) {
+				timer1_reset();
+				timing = 1;
+			}
+
+			inp_2_ref = timer1_get();
+			inp_2_set = 1;
+		}
+
+		if (inp_0_set && inp_1_set && inp_2_set) {
+			// Process and display result
+			stdout = &uart_output;
+			fprintf(stdout, "TimeDiffRaw: inp0=%i inp1=%i inp2=%i\n", inp_0_ref, inp_1_ref, inp_2_ref);
+
+			// Reset variables to default
+			inp_0_ref = 0;
+			inp_1_ref = 0;
+			inp_2_ref = 0;
+			inp_0_set = 0;
+			inp_1_set = 0;
+			inp_2_set = 0;
+			timing = 0;
+
+			// Sleep for 2s
+			_delay_ms(2000);
+		}
+	}
+}
+
 int main(void) {
 	// Initializations
 	uart_init();
 	stdout = &uart_output;
 	adc_init();
+	timer1_init();
 
 	puts("Initialized!");
 
-	// XXX Time meassurement
-	DDRB |= _BV(PB5); // Set arduino LED pin 13 to output 
-
-	while(1) {
-		PORTB |= _BV(PB5);  // High
-		recordInpbuf(0);
-		PORTB &= ~_BV(PB5); // Low
-		_delay_ms(5000);
-	}
+	distMeter();
 
 	return 0;
 }
